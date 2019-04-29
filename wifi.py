b0f34c1d555deac1d37adfc1e1eed0c3d8f80272
Graham Boyer <grahamboyer@D-10-16-13-235.dhcp4.washington.edu>
2019-04-29 14:50:22 -0700

    wifi python script basic

---
+++ wifi.py
@@ -0,0 +1,35 @@
+import socketserver
+import serial
+
+class MyTCPHandler(socketserver.BaseRequestHandler):
+    """
+    The request handler class for our server.
+    It is instantiated once per connection to the server, and must
+    override the handle() method to implement communication to the
+    client.
+    """
+
+    def handle(self):
+        # self.request is the TCP socket connected to the client
+        self.data = self.request.recv(1024).strip()
+        
+        #continuous saving the data in the txt file (everytime line 18 is excecuted the program emptry out the txt file)
+        
+        file =  open("/home/pi/TCP_test/log.txt", "ab")
+        file.write((self.data))
+        file.write(b'\n')
+        file.close()
+        print("{} wrote:".format(self.client_address[0]))
+        print(self.data)
+
+if __name__ == "__main__":
+    HOST, PORT = "10.16.30.219", 6969
+    print("Starting server......")
+    # Create the server, binding to localhost on port 9999
+    server = socketserver.TCPServer((HOST, PORT), MyTCPHandler)
+    print("here")
+    # Activate the server; this will keep running until you
+    # interrupt the program with Ctrl-C
+    server.serve_forever()
+    print("forever")
+    
