24d8fb5f3036010a0e7d247310e39a0ba68127d9
TacoMeatless <gboyer253@gmail.com>
2019-04-30 17:32:55 -0700

    removed somethings

--- blink_sketfh/blink_sketfh.ino
+++
@@ -1,8 +0,0 @@
-#define BME_SCK 13
-#define BME_MISO 12
-#define BME_MOSI 11
-#define BME_CS 10
-
-Adafruit_BME680 bme; // I2C
-//Adafruit_BME680 bme(BME_CS); // hardware SPI
-//Adafruit_BME680 bme(BME_CS, BME_MOSI, BME_MISO,  BME_SCK);
