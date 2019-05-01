dc849e418ef29c15d5b0258054a8ece516fb6ce5
Graham Boyer <grahamboyer@Grahams-MacBook-Pro-2.local>
2019-04-30 20:57:39 -0700

    update

--- GPS_test/src/main.cpp
+++ GPS_test/src/main.cpp
@@ -1,9 +1,16 @@
 #include <Arduino.h>
 
+#define RXD2 16
+
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(115200);
+  Serial2.begin(9600,SERIAL_8N1, RXD2);
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+ 
+  while ( Serial2.available()) {
+    Serial.print(char(Serial2.read()));
+  }
 }
