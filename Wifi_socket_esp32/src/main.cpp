bf5062fc579edaa28e95b39ae336b435d1a943e2
Graham Boyer <grahamboyer@D-10-16-22-255.dhcp4.washington.edu>
2019-05-05 12:26:39 -0700

    files added

--- Wifi_socket_esp32/src/main.cpp
+++ Wifi_socket_esp32/src/main.cpp
@@ -1,71 +1,29 @@
+#include <wifi_transmit.h>
 #include <Arduino.h>
-#include <WiFi.h>
 
-const char* ssid     = "University of Washington";
-const char* password = "";
-int nodeNumber = 0;
-double temp = 0;
-const char* host = "10.16.30.219";
-//const char* streamId   = "....................";
-//const char* privateKey = "....................";
+#define LED 5
 
-void setup()
-{
-    Serial.begin(115200);
-    delay(10);
+byte state = LOW;
+wifi_transmit test;
 
-    // We start by connecting to a WiFi network
-
-    Serial.println();
-    Serial.println();
-    Serial.print("Connecting to...");
-    Serial.println(ssid);
-
-    WiFi.begin(ssid, password);
-
-    while (WiFi.status() != WL_CONNECTED) 
-    {
-        delay(500);
-        Serial.print(".");
-    }
-
-    Serial.println("");
-    Serial.println("WiFi connected");
-    Serial.println("IP address: ");
-    Serial.println(WiFi.localIP());
+void led_state(){
+  state = !state;
+  digitalWrite(LED, state);
 }
 
-void loop()
-{
-    Serial.println("WiFi connected :: Start sending data to the server");
-    Serial.println("IP address: ");
-    Serial.println(WiFi.localIP());
-    
-    delay(1000);
-    Serial.print("connecting to ");
-    Serial.println(host);
 
-    // Use WiFiClient class to create TCP connections
-    WiFiClient client;
-    const int httpPort = 6969;
-    if (!client.connect(host, httpPort)) 
-    {
-        Serial.println("connection failed");
-        return;
-    }
-
-    //This will send the request to the server
-    client.print(String("GET: Hello World!!!, ")+"Host: " + host + ", " + "Connection: "+ httpPort);
-    //client.print(nodePrint+"\n"+SensorPrint+"\n"+PrintValue);
-    unsigned long timeout = millis();
-    while (client.available() == 0) {
-        if (millis() - timeout > 5000) {
-            Serial.println(">>> Client Timeout !");
-            client.stop();
-            return;
-        }
-    }
-
-    Serial.println();
-    Serial.println("closing connection");
+void setup() {
+    Serial.begin(111500);
+    pinMode(LED, OUTPUT);    
+    delay(3000);
+    Serial.print("Begin Test");
 }
+
+void loop() {
+    led_state();
+    test.send_message("hello world");
+    delay(3000);
+
+}
+
+
